d857e6a1010bcf0d50d9f2acdf530770a832027f
Matthew Behan-Fossey <matthewbf8@gmail.com>
2020-05-25 17:16:24 -0600

    Added a basic users request

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -6,8 +6,18 @@ const router = express.Router();
 // Load User model
 const User = require("../Models/User.model.js");
 
-router.post("/hello", (req, res) => {
-  return "hello"
+
+router.get("/", (req, res) => {
+  console.log("req to /")
+  User.find({}).then(users => {
+    if(users){
+      console.log(users)
+      return res.status(200).json({message: "Got Users"})
+    } else {
+      console.log("error retreiving users")
+      return res.status(500).json({message: "Could not get users."})
+    }
+  })
 });
 
 module.exports = router;
